63d216083a77c14a298d3f2118aaaf008ea50ed5
Lola Quiroga <165321584+ljquirog@users.noreply.github.com>
2024-08-26 11:14:30 -0700

    Add files via upload

--- paracruz_otp_review.R
+++ paracruz_otp_review.R
@@ -4,12 +4,27 @@ library(dplyr)
 library(readxl)
 library(writexl)
 
-data <- read_excel("your_file.xlsx")
+data <- read_excel("C:/Users/Lola/Documents/SCMTD/PARACRUZ_MAP/Completed Rides Jan through June 2024 (1).xlsx")
 
+pick_up_counts <- data %>%
+  group_by(`Pick-Up Address`) %>%
+  summarise(
+    `Pick-Up Zipcode` = first(`Pick-Up Zipcode`),
+    `Pick-Up City` = first(`Pick-Up City`),
+    `Number of Pick-Ups` = n()
+  )
 
-result <- data %>%
-  group_by(Address) %>%
-  summarise(Trips = n())
+drop_off_counts <- data %>%
+  group_by(`Drop-Off Address`) %>%
+  summarise(`Number of Drop-Offs` = n())
 
-write_xlsx(result, "cleaned_addresses.xlsx")
+summary_data <- pick_up_counts %>%
+  left_join(drop_off_counts, by = c("Pick-Up Address" = "Drop-Off Address")) %>%
+  rename(`Address` = `Pick-Up Address`) %>%
+  mutate(`Number of Drop-Offs` = ifelse(is.na(`Number of Drop-Offs`), 0, `Number of Drop-Offs`),
+         `Total Trips` = `Number of Drop-Offs` + `Number of Pick-Ups`)
   
+
+
+write_xlsx(summary_data, "C:/Users/Lola/Documents/SCMTD/PARACRUZ_MAP/paracruz_cleaned_addresses.xlsx")
+
